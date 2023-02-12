46060e9968b5ff1db8d9f74e858bd39d614a8709
João_Paulo <jpbbarboza1987@gmail.com>
2023-02-12 15:13:02 -0300

    Atualização da página Home

--- Home.py
+++ Home.py
@@ -35,8 +35,8 @@ st.markdown("""
         Growth Dashboard was maked to keep tracking the restaurants and delivery person metrics and growth.
 
         ### How to use this Dashboard?
-        - Company view:
-            - Managerial view: Behavior general metrics.
+        *####- Company view:*
+            *#####- Managerial view: Behavior general metrics.*
             - Strategic view: Weekly growth indicators. 
             - Geographical view: Geolocation insights.
         - Delivery person view: 
@@ -44,7 +44,7 @@ st.markdown("""
         - Restaurants view:
             - weekly restaurant growth indicators.
         ### Ask for help:
-            - https://www.linkedin.com/in/joao-paulo-barboza/
+         https://www.linkedin.com/in/joao-paulo-barboza/
         """)
 
 
