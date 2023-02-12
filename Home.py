0ab0bdcab110dd3b60061a70b4046aea3865cd9e
João_Paulo <jpbbarboza1987@gmail.com>
2023-02-12 15:08:14 -0300

    Atualização da página Home

--- Home.py
+++ Home.py
@@ -26,3 +26,37 @@ df2 = utils.clean_code(df2)
 df2 = utils.create_sidebar(df2)
 
 #============================================================
+
+
+
+st.write('# Curry Company Growth Dashboard')
+
+st.markdown("""
+        Growth Dashboard was maked to keep tracking the restaurants and delivery person metrics and growth.
+
+        ### How to use this Dashboard?
+        - Company view:
+            - Managerial view: Behavior general metrics.
+            - Strategic view: Weekly growth indicators. 
+            - Geographical view: Geolocation insights.
+        - Delivery person view: 
+            - monitoring of weekly growth indicators.
+        - Restaurants view:
+            - weekly restaurant growth indicators.
+        ### Ask for help:
+            - https://www.linkedin.com/in/joao-paulo-barboza/
+        """)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
